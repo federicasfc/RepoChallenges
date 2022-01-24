576ad86af1ceb21b071f22ec44da61b3c81cb726
federicasfc <fesafeco@fakeemail.com>
2022-01-24 17:35:17 -0500

    Added some notes to DisplayAllBadges in UI; No super conclusive way to build it out yet

--- BadgeChallenge/BadgeUI/UserInterface.cs
+++ BadgeChallenge/BadgeUI/UserInterface.cs
@@ -127,6 +127,7 @@ namespace BadgeUI
 
             foreach (KeyValuePair<int, Badge> badge in dictOfBadges)
             {
+                //foreach for DoorNames or foreach through badge.Value depending on what I want printed out, which still needs to be figured out
                 Console.WriteLine("key = {0}, Value = {1}", badge.Key, badge.Value);
                 //Not using print content, but should at least do something. Need to edit PrintContent at some point.
                 //Also will print all of badge, not just doornames
